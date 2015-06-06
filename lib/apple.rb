5e20f3494e7e7d36e5e936aa407dd28eaf061e74
AnnaErshova <anna.ershova@gmail.com>
2015-06-05 22:45:59 -0400

    all done

--- lib/apple.rb
+++ lib/apple.rb
@@ -1,10 +1,26 @@
 def apple_picker_with_select(fruit_list)
   # fruit_list will look something like: ["apple", "banana", "apple"]
   # you want to return an array that just has apples in it
+  fruit_list.select { |fruit| fruit == "apple" }
+  # select maps all elements, iterates over them, and then returns a modified array, which is exactly what we want.
+  # here, it only returns fruits that are equal to apple
+  # doesn't modify the actual array
+  # selection |item| condition == what needs to be done; if nothing is done, then returns the array of items satisfying condition
 end
 
 def apple_picker_with_collect(fruit_list)
   # fruit_list will look something like: ["apple", "banana", "apple"]
   # you want to return an array that just the apples, just like the 
   # challenge above
+  fruit_list.collect {|fruit| fruit == "apple"? fruit : nil }.compact
+  # cannot use return apple because it stops it and does not keep forming the array
+  # can use .compact on the end word when it is an outcome of the block
+  # fruit_list.collect {|fruit| fruit == "apple"} will return an array that looks like [true true false false true]
+  # so we need to check is the value is true, and replace the true values with "apple" and replace false values with nil
+  # which we then can remove with compacting it
+  # .compact is actually great for this because it returns a copy of this array without nils
+
 end
+
+# alternatively, we could count number of apples and then return an instance of array where we enter apples as many times as we have counted
+
--- lib/holiday.rb
+++ lib/holiday.rb
@@ -16,29 +16,39 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above 
   # add the second argument, which is a supply, to either the
-  # Christmas or the New Year's arrays
+  # Christmas or the New Year's arrays -- the prompt should really be to BOTH? according to rspec at least
+
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
+    # is there a more efficient way to push into the keys?ß
 end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  holiday_hash[season][holiday_name] = supply_array
+  return holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  # has all the winter supplies (Lights, Wreath, Party Hats)
+  holiday_hash[:winter].values.flatten
 end
 
-def all_supplies_in_holidays(holiday_hash)
+# def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
@@ -46,9 +56,37 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+
+# first, iterate through the keys of holiday_hash to get to seasons
+# then, within that, iterate through the keys of seasons to get to holidays
+# then, within that, iterate through the holidays to get to supplies
+# convert the outcome to separate words in strings where all words are capitalized
+def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.keys.each do |season|
+    puts "#{season.capitalize}:"
+    holiday_hash[season].each do |holiday, supply|
+      supplies = supply.join(", ")
+      puts "  #{holiday.to_s.gsub("_"," ").split.map(&:capitalize).join(' ')}: #{supplies}"
+      # thank you, Stack Overflow! I have no real idea what &:capitalize does...
+    end
+  end
 end
 
+
 def all_holidays_with_bbq(holiday_hash)
+  new_array =[]
+  holiday_hash.each do |season, supply|
+    supply.each do |holiday, supplies|
+      if supplies.include?("BBQ")
+        new_array << holiday
+      end
+    end
+
+  end
   # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+  # include the string "BBQ" => BBQ is going to be in an array, so we will need to return that array
+  # get to the supply first then check
+  new_array
 end
+
+ 
